08e1c77eec1acc9cea83bf66d76ca2aece0dc219
Amoy N <moyanich@users.noreply.github.com>
2021-09-29 14:36:03 -0500

    Route redirect for users

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,73 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+*/
+
+
+Route::get('/dashboard', function () {
+    if (session('status')) {
+        return redirect()->route('admin.index')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.index');
+});
 
 require __DIR__.'/auth.php';
 
+Route::group([
+    'prefix' => 'user',
+    'as' => 'user.',
+    'namespace' => 'User',
+    'middleware' => ['auth']
+], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+});
 
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.',
+    'namespace' => 'Admin',
+    'middleware' => ['auth', 'admin']
+], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+   
+   
+   /* // Permissions
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+    Route::resource('permissions', 'PermissionsController');
+
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+    // Countries
+    Route::delete('countries/destroy', 'CountriesController@massDestroy')->name('countries.massDestroy');
+    Route::resource('countries', 'CountriesController');
+
+    // Cities
+    Route::delete('cities/destroy', 'CitiesController@massDestroy')->name('cities.massDestroy');
+    Route::resource('cities', 'CitiesController');
+
+    // Trips
+    Route::delete('trips/destroy', 'TripsController@massDestroy')->name('trips.massDestroy');
+    Route::resource('trips', 'TripsController'); */
+});
+
+
+
+
+
+
+/*
 Route::group(['prefix' => 'admin' , 'middleware' => ['auth']], function() {
     Route::get('dashboard', [AdminController::class. 'index'])->name('admin.dashboard');
     Route::get('profile', [AdminController::class. 'iprofile'])->name('admin.profile');
@@ -44,12 +104,7 @@ Route::group(['prefix' => 'user' , 'middleware' => ['auth']], function() {
     Route::get('settings', [UserController::class. 'iprofile'])->name('user.settings');
 });
 
+*/
 
-
-Route::prefix('admin')->group(function () {
-    
-
-
-});
 
 
