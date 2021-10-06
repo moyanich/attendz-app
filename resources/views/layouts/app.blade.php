846cc8c702dd5a543daaee1667cee9fa91d9854e
Amoy N <moyanich@users.noreply.github.com>
2021-10-06 10:18:55 -0500

    Include Modal Scripts in Layout.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,5 +40,32 @@
                 </main>
             </div>
         </div>
+
+        {{-- Modal Scripts --}}
+        <script>
+            function closeAlert(event){
+                let element = event.target;
+                while(element.nodeName !== "BUTTON"){
+                element = element.parentNode;
+                }
+                element.parentNode.parentNode.removeChild(element.parentNode);
+            }
+        </script>
+
+        <script type="text/javascript">
+            function toggleModal(modalID){
+                document.getElementById(modalID).classList.toggle("hidden");
+                document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
+                document.getElementById(modalID).classList.toggle("flex");
+                document.getElementById(modalID + "-backdrop").classList.toggle("flex");
+            }
+        </script>
+        {{-- End Modal Scripts --}}
+
+
     </body>
 </html>
+
+
+
+
