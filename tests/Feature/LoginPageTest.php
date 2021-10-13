654cd2355ab37a17163e93c41a8bd9d4955c39de
Amoy N <moyanich@users.noreply.github.com>
2021-10-13 15:24:58 -0500

    Update LoginPageTest.php

--- tests/Feature/LoginPageTest.php
+++ tests/Feature/LoginPageTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class LoginPageTest extends TestCase
 {
@@ -13,10 +14,42 @@ class LoginPageTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_can_login_using_the_login_form()
     {
-        $response = $this->get('/');
+        $user = USER::factory()->create();
 
-        $response->assertStatus(200);
+        $response = $this->post('/login' , [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        ]);
+
+        $this->assertAuthenticated();
+
+        $response->assertRedirect('/');
+    }
+
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function admin_user_can_login_using_the_login_form()
+    {
+        $user = USER::factory()->create();
+
+        $response = $this->post('/login' , [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
+        ]);
+
+
+        $this->get('/admin/user');
+
+        $this->assertAuthenticated();
+
+        $response->assertRedirect('/');
     }
 }
