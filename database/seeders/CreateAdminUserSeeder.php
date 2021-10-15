f3babbfa352694330a7da11a392f2da0d11b3c6c
Amoy N <moyanich@users.noreply.github.com>
2021-10-15 14:07:44 -0500

    Update CreateAdminUserSeeder.php

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 use App\Models\User;
 use Hash;
 
@@ -24,12 +25,35 @@ class CreateAdminUserSeeder extends Seeder
             'password' => 'password',
         ]);
 
-    
-       // $role = Role::create(['name' => 'Admin']);
+        //$user->roles()->sync([1, 2, 3]); // works
+
+        // testing
+        $role = Role::where('name', '=', 'Admin')
+        ->get();
+
+        $user->roles()->sync($role);
+
+        //dd($role);
      
        // $permissions = Permission::pluck('id','id')->all();
    
         //$role->syncPermissions($permissions);
+     
+       // $user->assignRole([$role->id]);
+
+       //$user->roles()->attach($role->id); */
+
+
+        //$role = Role::create(['name' => 'Admin']);
+     
+        //$role = Role::pluck('id','id')->all();
+
+   
+        //$role->sync($role);
+
+        //
+
+        
      
        // $user->assignRole([$role->id]);
     }
