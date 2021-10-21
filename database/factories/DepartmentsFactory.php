f98e834a8ac439955213371ec0a48f4fd8278411
Amoy N <moyanich@users.noreply.github.com>
2021-10-21 13:52:13 -0500

    Create departments resource

---
+++ database/factories/DepartmentsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Departments;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DepartmentsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Departments::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
