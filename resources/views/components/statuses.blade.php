05160e077769eb89a335240da10353978521c6c1
Amoy N <moyanich@users.noreply.github.com>
2022-01-17 13:09:25 -0500

    Update statuses.blade.php
    Refactor code to use a switch statement

--- resources/views/components/statuses.blade.php
+++ resources/views/components/statuses.blade.php
@@ -2,9 +2,40 @@
     $classes = 'px-2 flex text-sm leading-5 font-semibold bg-transparent rounded-full items-center';
 @endphp
 
+{{--  Refactored to use switch --}}
+@switch($message)
 
-// TODO: REFACTOR TO USE A SWITCH
+    @case('active')
+        <div {{ $attributes->merge(['class' => $classes . ' text-emerald-500']) }}>
+            <span class="badge badge-primary px-2 py-1 text-xs font-bold mr-1">{{ $slot }}</span>
+        </div>
+        @break
 
+    @case('inactive')
+        <div {{ $attributes->merge(['class' => $classes . ' text-blueGray-800']) }}>
+            <span class="badge badge-accent px-2 py-1 text-xs font-bold mr-1">{{ $slot }}</span>
+        </div>
+        @break
+    
+    @case('new')
+        <div {{ $attributes->merge(['class' => $classes . ' text-blue-400']) }}>
+            <span class="badge badge-secondary px-2 py-1 text-xs font-bold mr-1">{{ $slot }}</span>
+        </div>
+        @break
+
+     @case('')
+        @break
+    
+    @default
+        <div {{ $attributes->merge(['class' => $classes . ' text-blueGray-800']) }}>
+            <span class="badge px-2 py-1 text-xs font-bold mr-1">{{ $slot }}</span>
+        </div>
+        @break
+
+@endswitch
+
+
+{{--  
 @if ($message == 'active')
 
     <div {{ $attributes->merge(['class' => $classes . ' text-emerald-500']) }}>
@@ -25,7 +56,6 @@
 
 @elseif ($message == '')
 
-
 @else 
 
     <div {{ $attributes->merge(['class' => $classes . ' text-blueGray-800']) }}>
@@ -33,3 +63,5 @@
     </div>
 
 @endif
+--}}
+
